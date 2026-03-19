ed51c694a6b189fbcb7e6fef0d99c0e9abe165f3
Shihab Mian <39349212+808sheeb@users.noreply.github.com>
2026-03-19 00:08:38 -0400

    Updates - Inner Working

--- scripts/audio_controller.gd
+++ scripts/audio_controller.gd
@@ -1,10 +1,15 @@
 extends Node
 var isAlarmGoingOff := false
 @export var mute: bool = false
+var playBackgroundMusic := true
+var trackNo = 0
 
 func _ready() -> void:
 	GlobalMessenger.connect("ALARM_SNOOZE", stop_alarm)
 	GlobalMessenger.connect("ALARM_TIMEOUT", play_alarm)
+	if playBackgroundMusic:
+		if trackNo == 0:
+			$bg1.play()
 
 func play_alarm() -> void:
 	if not mute:
