9241c4a5677285215fdfbf6456bd56a4b1044cd1
Shihab Mian <39349212+808sheeb@users.noreply.github.com>
2026-03-31 02:42:24 -0400

    Keys Update + Playtest Bugfix

--- scripts/scene/living_room.gd
+++ scripts/scene/living_room.gd
@@ -1,12 +1,9 @@
 extends Node3D
-var keyw = preload("uid://celmha118kao6").instantiate()
+
 
 func _ready() -> void:
 	#print(self)
-	GlobalMessenger.connect("SPAWN_KEYS", spawn)
-	keyw.transform.origin = Vector3(-1.164,0.939,0.336)
-	keyw.rotation_degrees = Vector3(0, 56.5, 0)
-	self.add_child(keyw)
+	pass
 
 func spawn():
 	print("Spawn")
--- scripts/scene/shihab_room.gd
+++ scripts/scene/shihab_room.gd
@@ -3,12 +3,15 @@ extends Node3D
 @onready var flavor_text: Sprite3D = $FlavorText
 var keyIsActive = false
 
+var keyw = preload("uid://celmha118kao6").instantiate()
 
 #THERE IS A PROBLEM HERE FIX THIS
 #i think i fixed 
 func _ready() -> void:
 	GlobalMessenger.connect("KEY_WINTER", activeToggle)
 	GlobalMessenger.connect("ALARM_SNOOZE", alarmSnooze)
+	
+	GlobalMessenger.connect("SPAWN_KEYS", spawn)
 	
 func _process(_delta: float) -> void:
 	if keyIsActive:
@@ -25,3 +28,9 @@ func activeToggle():
 
 func alarmSnooze():
 	keyIsActive = false
+	
+func spawn():
+	if keyIsActive:
+		keyw.transform.origin = Vector3(6.256,1.711,-18.685)
+		keyw.rotation_degrees = Vector3(0, -62.6, 0)
+		self.add_child(keyw)
